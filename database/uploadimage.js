ab5644c3d9d967d5a2a0d29000cddbcdfba57800
priyanka verma <priyanka2050@outlook.com>
2023-04-16 22:45:37 +0530

    update  image endpoint

--- database/uploadimage.js
+++ database/uploadimage.js
@@ -8,25 +8,31 @@ require("dotenv").config();
 //   metadata: { reviewer: 'john', reviewDate: '2022-04-01' }, 
 //   tags: {project: 'xyz', owner: 'accounts-payable'}
 // }
-async function uploadImage(containerName,blobName, localFileWithPath, uploadOptions) {
-  
-    try {
-      const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
-      if (!accountName) return ('{"error":"Azure Storage accountName not found"}');
-      
-      const blobServiceClient  = BlobServiceClient.fromConnectionString(process.env.STORAGE_CONNECTION_STRING);
-      // Get a reference to a container
-      const containerClient = blobServiceClient.getContainerClient(containerName);
-      const blockBlobClient = await containerClient.getBlockBlobClient(blobName);
-  
-      // upload file to blob storage
-      var uploadResult = await blockBlobClient.uploadFile(localFileWithPath, uploadOptions);
-      return(`{"message":"${blobName} succeeded","url":"${blockBlobClient.url}"}`);
-  
-    } catch (err) {
-      return(`{"error": "${err.message}"}`);
-    }
-   
+async function uploadFile(containerName, blobName, localFileWithPath, uploadOptions) {
+
+  try {
+    const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+    if (!accountName) return ('{"error":"Azure Storage accountName not found"}');
+
+    const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.STORAGE_CONNECTION_STRING);
+    // Get a reference to a container
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    
+      const options = {
+        access: 'blob'
+      };
+      var response = await containerClient.createIfNotExists(options);   
+
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    //blockBlobClient.createIfNotExists();
+    // upload file to blob storage
+    var uploadResult = await blockBlobClient.uploadFile(localFileWithPath, uploadOptions);
+    return (`{"message":"${blobName} succeeded","url":"${blockBlobClient.url}"}`);
+
+  } catch (err) {
+    return (`{"error": "${err.message}"}`);
+  }
+
 }
- module.exports = {uploadImage};
+module.exports = { uploadFile };
 
--- routes/images-endpoint.js
+++ routes/images-endpoint.js
@@ -2,8 +2,12 @@
 var express = require('express');
 var router = express.Router();
 var uploadBlob = require('../database/uploadimage')
-const bcrypt=require('bcrypt');
+const bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
+const ErrorResponse = require('../model/error');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+const bodyParser = require('body-parser');
 
 require("dotenv").config();
 
@@ -13,30 +17,46 @@ require("dotenv").config();
 //   metadata: { reviewer: 'john', reviewDate: '2022-04-01' }, 
 //   tags: {project: 'xyz', owner: 'accounts-payable'}
 // }
+router.use(bodyParser.urlencoded({ extended: true }));
 router.route('/upload')
-.post(async function(req, res) {  
-    try {
-        const { containerName, blobName, filePath, uploadOptions } = req.body;
-        if (!(containerName && blobName && filePath)) {
-            res.status(400).send("containerName, blobName, filePath is required");
-        }
-        var result= await uploadBlob.uploadImage(containerName,blobName,filePath,uploadOptions);
-        var response = JSON.parse(result);
-        if(response.error){
-            var responseError = ErrorResponse(500,'Internal server error',result.error);              
-            
-            res.status(500).send(responseError);
-        }
-        if(response.message)
-        {
-            res.status(200).send(response);
-        }
-        else
-            res.status(409).send(response);
+    .post(upload.single("picture"), async function (req, res) {
+        var errResponse;
+        try {
+            const { containerName, uploader, entityName } = req.body;
+            const filetoUpload = req.file;
+            const uploadOptions = {
+                metadata: {
+                    'uploader': uploader,
 
-    }catch (err) {
-        console.log(err);
-        res.status(500).send("Internal server error");
-    }  
-});
+                },
+                tags: {
+                    'project': containerName,
+                    'owner': entityName
+                }
+            };
+            if (!(containerName && filetoUpload)) {
+                errResponse = new ErrorResponse(400, "containerName, blobName, filePath is required", "");
+                res.status(400).json(errResponse);
+                return;
+            }
+            var result = await uploadBlob.uploadFile(containerName, filetoUpload.originalname, filetoUpload.path, uploadOptions);
+            var response = JSON.parse(result);
+            if (response.error) {
+                responseError = new ErrorResponse(500, 'Internal server error', result.error);
+
+                res.status(500).json(responseError);
+                return;
+            }
+            if (response.message) {
+                res.status(201).json(response);
+            }
+            else
+                res.status(409).json(response);
+            return;
+
+        } catch (err) {
+            errResponse = new ErrorResponse(500, "Internal server error", err);
+            res.status(500).json(errResponse);
+        }
+    });
 module.exports = router;
