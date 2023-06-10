9b65e4e7e5307e8e92a115e7ca4b237cb2d37c73
umeshpalav <umeshpalav96@gmail.com>
2023-06-11 00:11:17 +0530

    Single Level Project report Generation Completed

--- service/sectionParts/sectionPartProcessExecutorFactoryForSingleLevelProject.js
+++ service/sectionParts/sectionPartProcessExecutorFactoryForSingleLevelProject.js
@@ -1,28 +1,24 @@
 const ProjectReportType = require('../../model/projectReportType.js');
 const ProcessExecutor = require("./generateHtmlProcessExecutor.js");
-const GenerateSectionHeaderHTML = require("./header/generateSectionHeaderHtml.js");
 const GenerateVisualSectionHTML = require("./visualSection/generateVisualSectionHtml.js");
 const GenerateSectionInvasiveHTML = require("./invasiveOnly/generateSectionInvasiveOnly.js");
 const GenerateSectionConclusiveHTML = require("./conclusive/generateSectionConclusiveHtml.js");
 
-class SectionPartProcessExecutorFactoryForMultiLevelProject {
+class SectionPartProcessExecutorFactoryForSingleLevelProject {
     
-    getProcessExecutorChain(location,sectionName,sectionId,sectionImageProperties,reportType)
+    getProcessExecutorChain(sectionId,sectionImageProperties,reportType)
     {
         const processExecutor = new ProcessExecutor();
-        const generateSectionHeaderHTML = new GenerateSectionHeaderHTML(location,sectionName,reportType);
         const generateVisualSectionHtml = new GenerateVisualSectionHTML(sectionId,sectionImageProperties);
         const generateInvasiveSectionHtml = new GenerateSectionInvasiveHTML(sectionId,sectionImageProperties);
         const generateConclusiveSectionHtml = new GenerateSectionConclusiveHTML(sectionId,sectionImageProperties);
         if(ProjectReportType.VISUALREPORT === reportType)
         {
-            processExecutor.addProcess(generateSectionHeaderHTML);
             processExecutor.addProcess(generateVisualSectionHtml);
             return processExecutor;
         }
         else if(ProjectReportType.INVASIVEVISUAL === reportType)
         {
-            processExecutor.addProcess(generateSectionHeaderHTML);
             processExecutor.addProcess(generateVisualSectionHtml);
             processExecutor.addProcess(generateInvasiveSectionHtml);
             processExecutor.addProcess(generateConclusiveSectionHtml);
@@ -30,7 +26,6 @@ class SectionPartProcessExecutorFactoryForMultiLevelProject {
         }
         else if(ProjectReportType.INVASIVEONLY === reportType)
         {
-            processExecutor.addProcess(generateSectionHeaderHTML);
             processExecutor.addProcess(generateInvasiveSectionHtml);
             processExecutor.addProcess(generateConclusiveSectionHtml);
             return processExecutor;
@@ -38,4 +33,4 @@ class SectionPartProcessExecutorFactoryForMultiLevelProject {
     }
 }
 
-module.exports = new SectionPartProcessExecutorFactoryForMultiLevelProject();
+module.exports = new SectionPartProcessExecutorFactoryForSingleLevelProject();
