f4cc3e34a2d7445798be190b8d158f293281124c
umeshpalav <umeshpalav96@gmail.com>
2023-07-30 02:10:05 +0530

    Conclusive API's added

--- model/conclusiveSections.js
+++ model/conclusiveSections.js
@@ -3,12 +3,14 @@ var ObjectId = require('mongodb').ObjectId;
 const { QueryCollectionFormat } = require('@azure/core-http');
 const { JsonWebTokenError } = require('jsonwebtoken');
 var mongo = require('../database/mongo');
+const RatingMapping  = require("./ratingMapping.js");
+
 
 var getConclusiveSectionById = async function(id){
     var response = {};
     try {
         const result = await mongo.ConclusiveSections.findOne({ _id: new ObjectId(id) });
-
+        transformData(result);
         if (result) {
             response = {
                 "data": {
@@ -29,6 +31,7 @@ var getConclusiveSectionById = async function(id){
         }
     }
     catch (err) {
+        console.log(err);
         response = {
             "error": {
                 "code": 500,
@@ -41,12 +44,18 @@ var getConclusiveSectionById = async function(id){
 
 }
 
+
+var transformData = function(conclusiveSection) {
+    conclusiveSection.eeeconclusive = RatingMapping[conclusiveSection.eeeconclusive];
+    conclusiveSection.lbcconclusive = RatingMapping[conclusiveSection.lbcconclusive];
+    conclusiveSection.aweconclusive = RatingMapping[conclusiveSection.aweconclusive];
+};
 
 var getConclusiveSectionByParentId = async function(id){
     var response = {};
     try {
         const result = await mongo.ConclusiveSections.findOne({ parentid: new ObjectId(id) });
-
+        transformData(result);
         if (result) {
             response = {
                 "data": {
@@ -79,8 +88,87 @@ var getConclusiveSectionByParentId = async function(id){
 
 }
 
+var addConclusiveSection = async function(conclusiveSection){
+    var response = {};
+    try {
+        var result = await mongo.ConclusiveSections.insertOne(conclusiveSection);
+        var insertedId = result.insertedId;
+        if(insertedId){
+            response = {
+                "data": {
+                    "id": insertedId,
+                    "message": "Conclusive Section inserted Successfully",
+                    "code": 201
+                }
+            }
+        }
+        else {
+            response = {
+                "error": {
+                    "code": 500,
+                    "message": "No Section inserted."
+                }
+            }
+        }
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+var editConclusiveSection = async function(conclusiveSectionId,newConclusiveData)
+{
+    var response ={};
+    try{
+        const updateObject = { $set: newConclusiveData };
+        var result = await mongo.ConclusiveSections.updateOne({ _id: new ObjectId(conclusiveSectionId) },updateObject,{upsert:false});    
+        
+        if(result.modifiedCount<1){
+            response = {
+                "error": {
+                    "code": 401,
+                    "message": "No Conclusive Section found."
+                  }
+            }
+            return response;
+        } else{
+            if(result.modifiedCount==1){
+                response = {
+                    "data" :{                   
+                        "message": "Conclusive Section updated successfully.",
+                        "code":201
+                    }   
+                };
+                return response;
+            }           
+            else{
+                response = {
+                    "data" :{                    
+                        "message": "Failed to update the Conclusive details.",
+                        "code":409
+                    }   
+                };
+                return response;
+            }                   
+        }   
+    }
+    catch(err){
+        console.log(err);
+        response = {
+            "error": {
+                "code": 500,
+                "message": "Error fetching Conclusive.",
+                "errordata": err
+              }
+        }
+        return response;
+    }
+    
+};
 
 module.exports = {
     getConclusiveSectionById,
-    getConclusiveSectionByParentId
+    getConclusiveSectionByParentId,
+    addConclusiveSection,
+    editConclusiveSection
 };
