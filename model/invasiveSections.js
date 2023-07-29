ed605dcf0ced116f3af274ba6792174a7f101b63
umeshpalav <umeshpalav96@gmail.com>
2023-07-30 00:56:51 +0530

    Invasive Section API added

--- model/invasiveSections.js
+++ model/invasiveSections.js
@@ -41,6 +41,34 @@ var getInvasiveSectionById = async function(id){
     }
 }
 
+var addInvasiveSection = async function(invasiveSection){
+    var response = {};
+    try {
+        var result = await mongo.InvasiveSections.insertOne(invasiveSection);
+        var insertedId = result.insertedId;
+        if(insertedId){
+            response = {
+                "data": {
+                    "id": insertedId,
+                    "message": "Invasive Section inserted Successfully",
+                    "code": 201
+                }
+            }
+        }
+        else {
+            response = {
+                "error": {
+                    "code": 500,
+                    "message": "No Section inserted."
+                }
+            }
+        }
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 var getInvasiveSectionByParentId = async function(id){
     var response = {};
     try {
@@ -77,7 +105,59 @@ var getInvasiveSectionByParentId = async function(id){
     }
 }
 
+var editInvasiveSection = async function(invasiveSectionId,newInvasiveData)
+{
+    var response ={};
+    try{
+        const updateObject = { $set: newInvasiveData };
+        var result = await mongo.InvasiveSections.updateOne({ _id: new ObjectId(invasiveSectionId) },updateObject,{upsert:false});    
+        
+        if(result.modifiedCount<1){
+            response = {
+                "error": {
+                    "code": 401,
+                    "message": "No Invasive Section found."
+                  }
+            }
+            return response;
+        } else{
+            if(result.modifiedCount==1){
+                response = {
+                    "data" :{                   
+                        "message": "Invasive Section updated successfully.",
+                        "code":201
+                    }   
+                };
+                return response;
+            }           
+            else{
+                response = {
+                    "data" :{                    
+                        "message": "Failed to update the Invasive details.",
+                        "code":409
+                    }   
+                };
+                return response;
+            }                   
+        }   
+    }
+    catch(err){
+        console.log(err);
+        response = {
+            "error": {
+                "code": 500,
+                "message": "Error fetching Invasive.",
+                "errordata": err
+              }
+        }
+        return response;
+    }
+    
+};
+
 module.exports = {
     getInvasiveSectionById,
-    getInvasiveSectionByParentId
+    getInvasiveSectionByParentId,
+    addInvasiveSection,
+    editInvasiveSection
 };
